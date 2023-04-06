0f30bed893f0dd5067be31249181cec7cb9bc2b9
Torham <torhamdev@gmail.com>
2023-04-06 21:27:35 +0330

    Make image captcha

---
+++ captcha_tools/make_image.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+from find_fonts import get_all_fonts_path
+
+
+def make_captcha_image():
+
+    fonts = get_all_fonts_path()
+
+    img = Image.new(mode='RGB', size=(200, 100), color="#ffffff")
+
+    d = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype(fonts[0], 45)
+
+    d.text((20, 20), '20 + 20', fill=(0, 0, 0), font=font)
+
+    s = BytesIO()
+    img.save(s, 'png')
+    in_memory_file = s.getvalue()
+
+    with open("test.png", "wb") as f:
+        f.write(in_memory_file)
