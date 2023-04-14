b3f7f16d14a11c8c718c9ae46342028e9c768801
Torham <torhamdev@gmail.com>
2023-04-14 15:22:59 +0330

    Add users database model

---
+++ app/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: int = Column(Integer, primary_key=True, index=True)
+    name: str = Column(String(50), index=True)
+    email: str = Column(String(50), unique=True, index=True)
+    password: str = Column(String(50))
