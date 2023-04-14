83a3de6eb2afc4248fac484b333a5865b48b8dfb
Torham <torhamdev@gmail.com>
2023-04-14 15:22:33 +0330

    Add sqlite databse using sqlalchemy

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "sqlite:///../users.db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
