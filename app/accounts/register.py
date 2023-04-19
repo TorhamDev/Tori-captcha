11d99d3d0173ca01d69bfe389e5fcf2c9530fded
Torham <torhamdev@gmail.com>
2023-04-19 18:25:22 +0330

    Handling duplicate data in registering users

--- app/accounts/register.py
+++ app/accounts/register.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.models import Users
 from app.schema import UserCreate, UserDataResponse
 
@@ -7,6 +7,12 @@ router = APIRouter()
 
 @router.post("/register")
 def register(data: UserCreate) -> UserDataResponse:
+    user = Users.select().where(Users.email == data.email)
+
+    if user.exists():
+        raise HTTPException(status_code=400, detail="User with this email already exists.")
+
     user = Users(**dict(data))
     user.save()
+
     return data
