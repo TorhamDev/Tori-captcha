23ef6451ff4bc1c25fecdbcea566ccc05d65af0b
Torham <torhamdev@gmail.com>
2023-04-22 18:54:31 +0330

    Add get captcha api but uncomplite

--- app/captcha/captcha_apis.py
+++ app/captcha/captcha_apis.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 from app.captcha.captcha_tools import make_captcha_image
 from app.database import redis_db
 from app.utils import get_unique_id_for_redis, make_random_captcha_question
 from app.schema import CreateCaptchaResponse
+from ast import literal_eval
 
 
 router = APIRouter()
@@ -20,3 +22,12 @@ def create_captcha():
     redis_db.set(captcha_id, str({"image": captcha_image, "answer": answer}))
 
     return {"id": captcha_id}
+
+
+@router.get("/get-captcha/{captcha_id}/")
+def get_captcha(captcha_id):
+
+    captcha_in_redis = redis_db.get(captcha_id)
+    captcha_data = literal_eval(captcha_in_redis.decode())
+
+    return FileResponse(captcha_data["image"])
