b3734c14064c40c7cbb8dbb8903768a8abbf6436
Torham <torhamdev@gmail.com>
2023-04-27 22:10:18 +0330

    Add expired captcha or invalid id error

--- app/errors.py
+++ app/errors.py
@@ -17,3 +17,9 @@ class EmailOrPasswordInvalid(HTTPException):
     def __init__(self, status_code=None, detail=None, headers=None) -> None:
         self.status_code = 400
         self.detail = "Email or password is invalid."
+
+
+class CaptchaIsExpiredOrInvalidID(HTTPException):
+    def __init__(self, status_code=None, detail=None, headers=None) -> None:
+        self.status_code = 404
+        self.detail = "Invalid id. Maybe the captcha has expired."
