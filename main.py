7fc9c0173c7ee3bd9298d6f006976a0e82aa4761
Torham <torhamdev@gmail.com>
2023-05-06 20:35:47 +0330

    Add some description for project swagger

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from app.accounts.register import router as register_router
 from app.accounts.login import router as login_router
 from app.captcha.captcha_apis import router as captcha_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Tori Captcha",
+    description="A captcha system",
+    version="0.0.1"
+)
 
 app.include_router(register_router)
 app.include_router(login_router)
 app.include_router(captcha_router)
 
 
-@app.get("/")
+@app.get("/", tags=["root"])
 def root():
     return {"Hi": "Halo"}
